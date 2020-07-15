7d316341c1ab2cd0a8b732b0eace4ba24cc5dd75
Elio-yang <2925539854@qq.com>
2020-07-16 00:23:29 +0800

    cJSON project format and review

---
+++ cmake-build-debug/CMakeFiles/json.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json.dir/src/cJSON.c.obj"
+  "CMakeFiles/json.dir/src/test.c.obj"
+  "json.exe"
+  "json.exe.manifest"
+  "json.pdb"
+  "libjson.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
